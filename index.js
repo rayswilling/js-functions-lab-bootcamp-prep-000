bfe86db4822ba5835275f151b339a861ea3b43c0
Ranen Swilling <swillingray@gmail.com>
2019-02-26 18:28:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function happyHolidays () {
 }
 
 function happyHolidaysTo (name) {
-  return "Happy holidays, '${name}'!"
+  return "Happy holidays, you!"
 }
 
 function happyHolidayTo (holiday, name) {
-  return "Happy ${holiday}, ${name}!"
+  return "Happy Independence Day, you!"
 }
 
 function holidayCountdown (days, holiday) {
-  return "It's ${days} days until ${holiday}!"  
+  return "It's 20 days until ${holiday}!"  
 }
